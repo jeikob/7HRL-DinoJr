b99b9ca0698e888e52e2c1452aa4a321dba98480
Jacob Burritt <rexro@Heroine.local>
2019-09-28 22:02:07 -0400

    Version 1 after 7HRL

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject
+{
+    public int wallDamage = 1;
+    public int hpPerHeart = 1;
+    public float restartLevelDelay = 1f;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    private Animator animator;
+    public int hp;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+        hp = GameManager.instance.playerHp;
+        base.Start();
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.playerHp = hp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Handle User Input in here
+        // Handle User Input in here
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement != Vector2.zero)
+        {
+            //AttemptMove<Wall>(movement.x, movement.y);
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    // Fixed Update is called at a fixed time interval rather than being tied to framerate
+    void FixedUpdate()
+    {
+        // Handle Player Movement in here
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    }
+
+    protected override void AttemptMove <T> (float xDir, float yDir)
+    {
+        base.AttemptMove<T>(xDir, yDir);
+        //RaycastHit2D hit;
+        CheckIfGameOver();
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Exit")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+        else if (other.tag == "Food")
+        {
+            hp += hpPerHeart;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    private void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void LoseHp (int hpLoss)
+    {
+        hp -= hpLoss;
+        CheckIfGameOver();
+    }
+
+    private void CheckIfGameOver()
+    {
+        if (hp <= 0)
+            GameManager.instance.GameOver();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Handle User Input in here
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if(movement != Vector2.zero){
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    // Fixed Update is called at a fixed time interval rather than being tied to framerate
+    void FixedUpdate()
+    {
+        // Handle Player Movement in here
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    }
+}
